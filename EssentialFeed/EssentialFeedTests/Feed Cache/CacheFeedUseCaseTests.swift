ebb83b26f9cea4bd908ae75e274dd2988c63c154
Donald Dang <donald.dang14@gmail.com>
2025-01-25 12:39:53 -0500

    Does not delete cache upon local feed loader creation

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Donald Dang on 1/25/25.
+//
+
+import Testing
+
+struct CacheFeedUseCaseTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
