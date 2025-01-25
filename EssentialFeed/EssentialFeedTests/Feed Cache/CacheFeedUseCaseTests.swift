e51733899779515d6b04bc9ec3235125961ab80a
Donald Dang <donald.dang14@gmail.com>
2025-01-25 12:44:20 -0500

    Save command cached deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,11 +6,55 @@
 //
 
 import Testing
+import EssentialFeed
+import Foundation
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
+}
 
 struct CacheFeedUseCaseTests {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func test()  {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        #expect(store.deleteCachedFeedCallCount == 0)
+    }
+    
+    @Test func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        #expect(store.deleteCachedFeedCallCount == 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "", location: "" , imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com/")!
     }
 
 }
