2191edfba45ae456cf46764cc0c401507bea6b6f
Donald Dang <donald.dang14@gmail.com>
2025-01-27 15:11:24 -0500

    local feed loader doesn't message store upon creation before loading from the cache store

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Donald Dang on 1/27/25.
+//
+
+import Testing
+
+struct LoadFeedFromCacheUseCaseTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
