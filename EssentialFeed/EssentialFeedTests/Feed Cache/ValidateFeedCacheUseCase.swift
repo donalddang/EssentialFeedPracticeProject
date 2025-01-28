d09bc0c602f5fec2bd3064a6d76ceef7f92e2783
Donald Dang <donald.dang14@gmail.com>
2025-01-28 14:55:46 -0500

    create use case file and have does not message store upon creation test

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  ValidateFeedCacheUseCase.swift
+//  EssentialFeedTests
+//
+//  Created by Donald Dang on 1/28/25.
+//
+
+import Testing
+
+struct ValidateFeedCacheUseCase {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
