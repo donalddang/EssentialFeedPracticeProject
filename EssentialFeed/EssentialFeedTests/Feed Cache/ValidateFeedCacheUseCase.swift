45873963b4836eff4684bd4d29c107a6878ed936
Donald Dang <donald.dang14@gmail.com>
2025-01-28 15:01:44 -0500

    extract cache deletion side effect on retrieval error from the load method to validate cache method

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCase.swift
@@ -5,12 +5,37 @@
 //  Created by Donald Dang on 1/28/25.
 //
 
-import Testing
+import XCTest
+import EssentialFeed
 
-struct ValidateFeedCacheUseCase {
+class ValidateFeedCacheUseCase: XCTestCase {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_load_deleteCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache()
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
+    //-MARK: Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 
 }
