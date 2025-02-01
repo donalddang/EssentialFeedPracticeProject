e2e6e256c621408fc6afb59b3e09de4d447af8b2
Donald Dang <donald.dang14@gmail.com>
2025-02-01 14:37:05 -0500

    retrieving from empty cache delivers empty result

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  CodableFeedStoreTests.swift
+//  EssentialFeed
+//
+//  Created by Donald Dang on 2/1/25.
+//
+
+import Testing
+
+struct CodableFeedStoreTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
